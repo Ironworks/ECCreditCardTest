e5d4f8d3d766e823d7dc9cdcb6fe1dfe1bfa27c9
Trevor Doodes <tdoodes@ironworksmedialimited.co.uk>
2016-04-11 23:09:44 +0100

    Implemented a very basic UI to allow testing.

--- ECCreditCardTest/ViewController.m
+++ ECCreditCardTest/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "ECCreditCard.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *accountNumberTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 
+- (IBAction)verifyButtonPressed:(id)sender;
 @end
 
 @implementation ViewController
@@ -24,4 +28,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)verifyButtonPressed:(id)sender {
+    
+    if (self.accountNumberTextField.text.length == 0) {
+        return;
+    }
+    
+    ECCreditCard *cardToVerify = [[ECCreditCard alloc] initWithCreditCardAccountNumber:self.accountNumberTextField.text];
+    
+    NSError *error;
+    if ([cardToVerify isValidWithError:&error]) {
+        self.resultLabel.text = [NSString stringWithFormat:@"Card Number %@ is a valid %@ card",cardToVerify.accountNumber, cardToVerify.issuerName];
+    } else {
+        //Verification failed
+        if (error) {
+            self.resultLabel.text = [NSString stringWithFormat:@"Card number %@ is invalid. %@", cardToVerify.accountNumber, error.localizedDescription];
+
+        } else {
+            self.resultLabel.text = @"Card is invalid. Unknown error!";
+        }
+    }
+}
 @end
